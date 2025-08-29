bd6be07970e6f44c87316f679f76b977c06fcdb0
oktoala <okta.yoga.syah@gmail.com>
2025-08-29 16:06:24 +0800

    change routing to lazy loading

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Routes } from '@generouted/react-router'
+import { Routes } from '@generouted/react-router/lazy'
 import './index.css'
 import { Provider } from 'react-redux'
 import { store } from './lib/redux-store'
